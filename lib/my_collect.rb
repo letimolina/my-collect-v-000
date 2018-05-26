0b313f97199eae40ab221d0f8a54c97ab94e34fe
Leticia Molina <letimolina@gmail.com>
2018-05-26 19:02:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(collection)
+  if collection.length == 0
+    collection
+  else
+    collection_two = []
+    i = 0
+    while i < collection.length 
+    
+ collection_two << yield(collection[i])
+ i += 1
+ end
+ collection_two
+end
+end
 
